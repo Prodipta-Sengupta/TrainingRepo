9b4f859cf04a5bd72f9a56a120b3789e67e7bde9
Prodipta Sengupta <47535510+Prodipta-Sengupta@users.noreply.github.com>
2023-08-05 17:51:26 +0530

    Update GreetingService.java
    Added info

--- src/com/example/training1/GreetingService.java
+++ src/com/example/training1/GreetingService.java
@@ -1,4 +1,4 @@
- 
+
 package com.example.training1;
 
 import javax.ws.rs.Consumes;
@@ -7,52 +7,51 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-
 @Path("greet")
 public class GreetingService {
 	/**
-     * Default constructor. 
-     */
-    public GreetingService() {
-        // TODO Auto-generated constructor stub
-		//Added another comment to test build
-		//Comment 2
-    }
-
-
-    /**
-     * Retrieves representation of an instance of GreetingService
-     * @return an instance of String
-     */
-	@GET
-	@Produces("text/plain")
-	public String resourceMethodGET() { 
-		return "Greetings! welcome to Updated DevOps Tools Discussion";
+	 * Default constructor.
+	 */
+	public GreetingService() {
+		// TODO Auto-generated constructor stub
+		// Added another comment to test build
+		// Comment 2
 	}
 
+	/**
+	 * Retrieves representation of an instance of GreetingService
+	 * 
+	 * @return an instance of String
+	 */
+	@GET
+	@Produces("text/plain")
+	public String resourceMethodGET() {
+		return "Greetings! welcome to Updated DevOps Tools Discussion";
+	}
 
 	@GET
 	@Path("hello")
 	@Produces("text/plain")
-	public String resourceMethodGET2() { 
-		return "Hello! Welcome to Jenkins CICD";
+	public String resourceMethodGET2() {
+		return "Hello! Welcome to Jenkins CICD Pipeline";
 	}
-	
+
 	@GET
 	@Path("hi")
 	@Produces("text/plain")
-	public String resourceMethodGET3() { 
+	public String resourceMethodGET3() {
 		return "Hi! Welcome to Jenkins CICD course";
 	}
-	
+
 	/**
-     * PUT method for updating or creating an instance of GreetingService
-     * @content content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
+	 * PUT method for updating or creating an instance of GreetingService
+	 * 
+	 * @content content representation for the resource
+	 * @return an HTTP response with content of the updated or created resource.
+	 */
 	@PUT
 	@Consumes("text/plain")
-	public void resourceMethodPUT(String content) { 
+	public void resourceMethodPUT(String content) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException();
 	}
